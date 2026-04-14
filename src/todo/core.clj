2c3bd44b56b04a5b53d140eee3567a12dd389ecd
uchitsa <uchitsa@ya.ru>
2026-04-15 01:55:19 +0300

    wip

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -15,9 +15,20 @@
     (do (println "Your tasks:")
       (doseq [[idx task] (map-indexed vector @tasks)]
         (let [status (if (:done task) "✅" " ")]
-          (println (str (inc idx)) ". [" status "] " (:text task)))))))
+          (println (str (inc idx)) ". ["status"] " (:text task)))))))
 
-(defn toggle-tasks [])
+(defn toggle-tasks []
+  (show-tasks)
+  (println "Enter the number of task to change the state:")
+  (let [idx (read-line)]
+    (if (and
+         (number? (try (Integer/parseInt idx) (count @tasks) (catch Exception _ nil)))
+         (> (Integer/parseInt idx) 0)
+         (<= (Integer/parseInt idx) (count @tasks)))
+      (do
+        (swap! tasks update (dec (Integer/parseInt idx)) #(assoc % :done (not (:done %))))
+               (println "State is changed!"))
+        (println "Wrong number"))))
 
 (defn delete-task [])
 
@@ -28,6 +39,7 @@
     (println "Welcome to ToDo task tracker")
     (println "1. Add task")
     (println "2. Show tasks")
+    (println "3. Change the task state")
 
     (println "5. Quit")
 
@@ -36,6 +48,7 @@
       (cond
         (= choice "1") (add-task)
         (= choice "2") (show-tasks)
+        (= choice "3") (toggle-tasks)
         (= choice "5") (do (println "Bye bye!") (System/exit 0))
         :else          (println "Wrong choice"))
       (recur))))
