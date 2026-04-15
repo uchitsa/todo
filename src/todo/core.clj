af5e841e1f41b2c5033a12cc1a29455e833f8282
uchitsa <uchitsa@ya.ru>
2026-04-16 02:31:20 +0300

    works

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -15,7 +15,7 @@
     (do (println "Your tasks:")
       (doseq [[idx task] (map-indexed vector @tasks)]
         (let [status (if (:done task) "✅" " ")]
-          (println (str (inc idx)) ". ["status"] " (:text task)))))))
+          (println (str (inc idx)) ". [" status "] " (:text task)))))))
 
 (defn toggle-tasks []
   (show-tasks)
@@ -27,12 +27,25 @@
          (<= (Integer/parseInt idx) (count @tasks)))
       (do
         (swap! tasks update (dec (Integer/parseInt idx)) #(assoc % :done (not (:done %))))
-               (println "State is changed!"))
-        (println "Wrong number"))))
+        (println "State is changed!"))
+      (println "Wrong number"))))
 
-(defn delete-task [])
 
-(defn remove-index [])
+(defn remove-index [coll idx]
+  (concat (take idx coll) (drop (inc idx) coll)))
+
+(defn delete-task []
+  (show-tasks)
+  (println "Enter the number of task to delete:")
+  (let [idx (read-line)]
+    (if (and (number? (try (Integer/parseInt idx) (catch Exception _ nil)))
+             (> (Integer/parseInt idx) 0)
+             (<= (Integer/parseInt idx) (count @tasks)))
+      (do
+        (swap! tasks (fn [ts] (vec (remove-index ts (dec (Integer/parseInt idx))))))
+        (println "The task is deleted!"))
+      (println "Wrong number"))))
+
 
 (defn -main []
   (loop []
@@ -40,7 +53,7 @@
     (println "1. Add task")
     (println "2. Show tasks")
     (println "3. Change the task state")
-
+    (println "4. Delete the task")
     (println "5. Quit")
 
     (println "Choose the action:")
@@ -49,6 +62,7 @@
         (= choice "1") (add-task)
         (= choice "2") (show-tasks)
         (= choice "3") (toggle-tasks)
+        (= choice "4") (delete-task)
         (= choice "5") (do (println "Bye bye!") (System/exit 0))
         :else          (println "Wrong choice"))
       (recur))))
